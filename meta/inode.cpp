d4fb8509b384e67f5741db99d59d456132a5e8d5
CHOKYUJIN <bori19960@naver.com>
2021-02-24 02:16:14 +0900

    Fixed exception to public, Added getter/setter

--- meta/inode.cpp
+++ meta/inode.cpp
@@ -5,6 +5,29 @@
 
 using std::runtime_error;
 
+inode::no_entry::no_entry(const char *msg) : runtime_error(msg){
+
+}
+inode::no_entry::no_entry(const string &msg) : runtime_error(msg.c_str()){
+
+}
+const char *inode::no_entry::what(void)
+{
+	return runtime_error::what();
+}
+
+inode:: permission_denied::permission_denied(const char *msg) : runtime_error(msg){
+
+}
+inode:: permission_denied::permission_denied(const string &msg) : runtime_error(msg.c_str()){
+
+}
+const char *inode::permission_denied::what(void)
+{
+	return runtime_error::what();
+}
+
+
 inode::inode(uid_t owner, gid_t group, mode_t mode) : i_mode(mode), i_uid(owner), i_gid(group), i_nlink(1), i_size(0)
 {
 	struct timespec ts;
@@ -41,3 +64,24 @@ void inode::deserialize(unique_ptr<char> value)
 {
 	memcpy(this, value.get(), sizeof(inode));
 }
+
+// getter
+mode_t inode::get_mode(){return this->i_mode;}
+uid_t inode::get_uid(){return this->i_uid;}
+gid_t inode::get_gid(){return this->i_gid;}
+ino_t inode::get_ino() {return this->i_ino;}
+nlink_t inode::get_nlink() {return this->i_nlink;}
+off_t inode::get_size(){return this->i_size;}
+struct timespec inode::get_atime(){return this->i_atime;}
+struct timespec inode::get_mtime(){return this->i_mtime;}
+struct timespec inode::get_ctime(){return this->i_ctime;}
+
+// setter
+void inode::set_mode(mode_t mode){this->i_mode = mode;}
+void inode::set_uid(uid_t uid){this->i_uid = uid;}
+void inode::set_gid(gid_t gid){this->i_gid = gid;}
+void inode::set_ino(ino_t ino){this->i_ino = ino;}
+void inode::set_nlink(nlink_t nlink){this->i_nlink = nlink;}
+void inode::set_size(off_t size){this->i_size = size;}
+void inode::set_atime(struct timespec atime){this->i_atime = atime;}
+void inode::set_mtime(struct timespec mtime){this->i_mtime = mtime;}
--- meta/inode.hpp
+++ meta/inode.hpp
@@ -3,8 +3,12 @@
 
 #include <memory>
 #include <sys/stat.h>
+#include <stdexcept>
+#include <string>
 
 using std::unique_ptr;
+using std::runtime_error;
+using std::string;
 
 class inode {
 private:
@@ -20,11 +24,48 @@ private:
 	struct timespec	i_ctime;
 
 public:
+	class no_entry : public runtime_error {
+	public:
+		no_entry(const char *msg);
+		no_entry(const string &msg);
+		const char *what(void);
+	};
+
+	class permission_denied : public runtime_error {
+	public:
+		permission_denied(const char *msg);
+		permission_denied(const string &msg);
+		const char *what(void);
+	};
+
 	inode(uid_t owner, gid_t group, mode_t mode);
+	inode(const std::string &path);
 
 	void fill_stat(struct stat *s);
 	unique_ptr<char> serialize(void);
 	void deserialize(unique_ptr<char>);
+
+	// getter
+	mode_t get_mode();
+	uid_t get_uid();
+	gid_t get_gid();
+	ino_t get_ino();
+	nlink_t get_nlink();
+	off_t get_size();
+	struct timespec get_atime();
+	struct timespec get_mtime();
+	struct timespec get_ctime();
+
+	// setter
+	void set_mode(mode_t mode);
+	void set_uid(uid_t uid);
+	void set_gid(gid_t gid);
+	void set_ino(ino_t ino);
+	void set_nlink(nlink_t nlink);
+	void set_size(off_t size);
+	void set_atime(struct timespec atime);
+	void set_mtime(struct timespec mtime);
+
 };
 
 #endif /* _INODE_HPP_ */
