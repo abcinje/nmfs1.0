c90a639c170b6c68d8ddab2bc9c9342072bf42c1
CHOKYUJIN <bori19960@naver.com>
2021-02-28 02:06:56 +0900

    Add first field

--- meta/file_handler.cpp
+++ meta/file_handler.cpp
@@ -1,5 +1,9 @@
-//
-// Created by snu-csl on 2021-02-24.
-//
-
 #include "file_handler.hpp"
+
+file_handler::file_handler(ino_t ino) : ino(ino) {
+
+}
+
+void file_handler::set_fhno(void *fhno) {
+	this->fhno = reinterpret_cast<uint64_t>(fhno);
+}
--- meta/file_handler.hpp
+++ meta/file_handler.hpp
@@ -1,9 +1,17 @@
 #ifndef NMFS0_FILE_HANDLER_HPP
 #define NMFS0_FILE_HANDLER_HPP
 
+#include <memory>
+#include <sys/stat.h>
 
 class file_handler {
+	ino_t ino;
+	uint64_t fhno;
 
+public:
+	file_handler(ino_t ino);
+
+	void set_fhno(void *fhno);
 };
 
 
