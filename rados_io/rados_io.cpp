37ddc7eb0f604fe60d10a48fb3076312fb3d9805
Injae Kang <abcinje@gmail.com>
2021-02-28 20:41:15 +0900

    Acquire locks for object I/O operations

--- rados_io/rados_io.cpp
+++ rados_io/rados_io.cpp
@@ -1,6 +1,9 @@
 #include "rados_io.hpp"
-
 #include "../logger/logger.hpp"
+
+#include <vector>
+
+using std::vector;
 
 #define MIN(a, b) ((a) < (b) ? (a) : (b))
 
@@ -16,9 +19,9 @@ size_t rados_io::read_obj(const string &key, char *value, size_t len, off_t offs
 	if (ret >= 0) {
 		global_logger.log("Read an object. (key: \"" + key + "\")");
 	} else if (ret == -ENOENT) {
-		throw no_such_object("rados_io::read() failed (key: \"" + key + "\")");
+		throw no_such_object("rados_io::read_obj() failed (key: \"" + key + "\")");
 	} else {
-		throw runtime_error("rados_io::read() failed");
+		throw runtime_error("rados_io::read_obj() failed");
 	}
 
 	memcpy(value, bl.c_str(), len);
@@ -37,7 +40,7 @@ size_t rados_io::write_obj(const string &key, const char *value, size_t len, off
 	if (ret >= 0) {
 		global_logger.log("Wrote an object. (key: \"" + key + "\")");
 	} else {
-		throw runtime_error("rados_io::write() failed");
+		throw runtime_error("rados_io::write_obj() failed");
 	}
 
 	return len;
@@ -52,6 +55,19 @@ rados_io::no_such_object::no_such_object(const string &msg) : runtime_error(msg.
 }
 
 const char *rados_io::no_such_object::what(void)
+{
+	return runtime_error::what();
+}
+
+rados_io::cannot_acquire_lock::cannot_acquire_lock(const char *msg) : runtime_error(msg)
+{
+}
+
+rados_io::cannot_acquire_lock::cannot_acquire_lock(const string &msg) :runtime_error(msg.c_str())
+{
+}
+
+const char *rados_io::cannot_acquire_lock::what(void)
 {
 	return runtime_error::what();
 }
@@ -100,48 +116,106 @@ rados_io::~rados_io(void)
 
 size_t rados_io::read(const string &key, char *value, size_t len, off_t offset)
 {
+	vector<string> locks;
+	string err_msg;
+	int ret = 0;
+
 	off_t cursor = offset;
 	off_t stop = offset + len;
-	size_t ret = 0;
+	size_t sum = 0;
 
+	/* lock phase */
+	for (uint64_t obj_num = (cursor >> OBJ_BITS); obj_num <= ((stop-1) >> OBJ_BITS); obj_num++) {
+		string obj_key = key + "$" + std::to_string(obj_num);
+		ret = ioctx.lock_shared(obj_key, obj_key, obj_key, obj_key, obj_key, nullptr, 0);
+
+		if (ret == -EBUSY) {
+			err_msg = "rados_io::read() failed (the lock is already held by another (client, cookie) pair)";
+			goto unlock;
+		} else if (ret == -EEXIST) {
+			err_msg = "rados_io::read() failed (the lock is already held by the same (client, cookie) pair";
+			goto unlock;
+		} else if (ret) {
+			err_msg = "rados_io::read() failed";
+			goto unlock;
+		}
+
+		locks.push_back(obj_key);
+	}
+
+	/* read phase */
 	while (cursor < stop) {
 		uint64_t obj_num = cursor >> OBJ_BITS;
 
 		off_t next_bound = (cursor & OBJ_MASK) + OBJ_SIZE;
 		size_t sub_len = MIN(next_bound - cursor, stop - cursor);
 
-		ret += this->read_obj(key + "$" + std::to_string(obj_num),
-				value + ret,
+		sum += this->read_obj(key + "$" + std::to_string(obj_num),
+				value + sum,
 				cursor & (~OBJ_MASK),
 				sub_len);
 
 		cursor = next_bound;
 	}
 
-	return ret;
+unlock:
+	/* unlock phase */
+	for (string &obj_key : locks)
+		ioctx.unlock(obj_key, obj_key, obj_key);
+
+	return ret ? 0 : sum;
 }
 
 size_t rados_io::write(const string &key, const char *value, size_t len, off_t offset)
 {
+	vector<string> locks;
+	string err_msg;
+	int ret = 0;
+
 	off_t cursor = offset;
 	off_t stop = offset + len;
-	size_t ret = 0;
+	size_t sum = 0;
 
+	/* lock phase */
+	for (uint64_t obj_num = (cursor >> OBJ_BITS); obj_num <= ((stop-1) >> OBJ_BITS); obj_num++) {
+		string obj_key = key + "$" + std::to_string(obj_num);
+		ret = ioctx.lock_exclusive(obj_key, obj_key, obj_key, obj_key, nullptr, 0);
+
+		if (ret == -EBUSY) {
+			err_msg = "rados_io::write() failed (the lock is already held by another (client, cookie) pair)";
+			goto unlock;
+		} else if (ret == -EEXIST) {
+			err_msg = "rados_io::write() failed (the lock is already held by the same (client, cookie) pair)";
+			goto unlock;
+		} else if (ret) {
+			err_msg = "rados_io::read() failed";
+			goto unlock;
+		}
+
+		locks.push_back(obj_key);
+	}
+
+	/* write phase */
 	while (cursor < stop) {
 		uint64_t obj_num = cursor >> OBJ_BITS;
 
 		off_t next_bound = (cursor & OBJ_MASK) + OBJ_SIZE;
 		size_t sub_len = MIN(next_bound - cursor, stop - cursor);
 
-		ret += this->write_obj(key + "$" + std::to_string(obj_num),
-				value + ret,
+		sum += this->write_obj(key + "$" + std::to_string(obj_num),
+				value + sum,
 				cursor & (~OBJ_MASK),
 				sub_len);
 
 		cursor = next_bound;
 	}
 
-	return ret;
+unlock:
+	/* unlock phase */
+	for (string &obj_key : locks)
+		ioctx.unlock(obj_key, obj_key, obj_key);
+
+	return ret ? 0 :sum;
 }
 
 bool rados_io::exist(const string &key)
