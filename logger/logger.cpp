052ce969bd4b8361ad209328e0f4ed23fe1bbc98
Injae Kang <abcinje@gmail.com>
2021-03-04 07:58:50 +0000

    Update logger

--- logger/logger.cpp
+++ logger/logger.cpp
@@ -1,29 +1,31 @@
 #include "logger.hpp"
 
+#define CYAN "\033[0;36m"
+#define RESET "\033[0m"
+
 void logger::log(enum code_location location, std::string message)
 {
-	std::string* location_str;
+	std::string location_str;
 
 	switch(location){
 		case fuse_op:
-			location_str = new std::string("fuse_operations");
+			location_str = "fuse_operations";
+			message = CYAN + message + RESET;
 			break;
 		case rados_io_ops:
-			location_str = new std::string("rados_io_ops");
+			location_str = "rados_io_ops";
 			break;
 		case dentry_ops:
-			location_str = new std::string("dentry_ops");
+			location_str = "dentry_ops";
 			break;
 		case inode_ops:
-			location_str = new std::string("inode_operations");
+			location_str = "inode_operations";
 			break;
 		default:
-			location_str = new std::string("Unknown");
+			location_str = "Unknown";
 	}
 
-	std::cout<< "[" << *location_str + "]\t " + message << std::endl;
-
-	free(location_str);
+	std::cout<< "[" + location_str + "]\t" + message << std::endl;
 }
 
 logger global_logger;
