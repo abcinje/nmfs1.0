587a39313033a837471c85e3dbb6adbe58272c86
CHOKYUJIN <bori19960@naver.com>
2021-03-31 15:59:22 +0900

    Implement become_leader() proto

--- lease/lease_util.cpp
+++ lease/lease_util.cpp
@@ -1,1 +1,18 @@
 #include "lease_util.hpp"
+
+shared_ptr<dentry_table> become_leader(ino_t directory_ino) {
+	shared_ptr<dentry_table> new_dentry_table = std::make_shared<dentry_table>(directory_ino);
+
+	new_dentry_table->set_loc(LOCAL);
+
+	fuse_context *fuse_ctx = fuse_get_context();
+	client *c = (client *) (fuse_ctx->private_data);
+	new_dentry_table->set_leader_id(c->get_client_id());
+
+	new_dentry_table->pull_child_metadata();
+	return new_dentry_table;
+}
+
+int retire_leader(ino_t directory_ino) {
+
+}
--- lease/lease_util.hpp
+++ lease/lease_util.hpp
@@ -2,8 +2,9 @@
 #define NMFS0_LEASE_UTIL_HPP
 
 #include <sys/stat.h>
+#include "../in_memory/dentry_table.hpp"
+#include "../client/client.hpp"
 
-int become_leader(ino_t directory_ino);
-
-
+shared_ptr<dentry_table> become_leader(ino_t directory_ino);
+int retire_leader(ino_t directory_ino);
 #endif //NMFS0_LEASE_UTIL_HPP
