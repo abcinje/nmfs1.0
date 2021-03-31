429163c978e2981b96a90d4cdf87d2f9c056a36d
CHOKYUJIN <bori19960@naver.com>
2021-03-31 17:00:29 +0900

    Implement member function

--- in_memory/dentry_table.cpp
+++ in_memory/dentry_table.cpp
@@ -1,20 +1,38 @@
 #include "dentry_table.hpp"
 
-dentry_table::dentry_table(ino_t dir_ino) : dir_ino(dir_ino){
-	pull_child_metadata(dir_ino);
+dentry_table::dentry_table(ino_t dir_ino, shared_ptr<inode> dir_inode) : dir_ino(dir_ino), dir_inode(dir_inode){
+	/*
+	 * if LOCAL : pull_child_metadata() right after return to caller
+	 * if REMOTE : don't call pull_child_metadata(), just add REMOTE info
+	 */
 }
 
 dentry_table::~dentry_table() {
 	this->child_inodes.clear();
 }
 
-
-shared_ptr<inode> dentry_table::get_dir_inode(){
-	if(this->loc == LOCAL) {
-		return this->dir_inode;
-	} else if (this->loc == REMOTE) {
-		/* TODO */
+int dentry_table::add_inode(std::string filename, shared_ptr<inode> inode){
+	auto ret = this->child_inodes.insert(std::make_pair(filename, nullptr));
+	if(ret.second) {
+		ret.first->second = inode;
+	} else {
+		global_logger.log(indexing_ops, "Already added file is tried to inserted");
+		return -1;
 	}
+	return 0;
+}
+
+int dentry_table::delete_inode(std::string filename) {
+	std::map<std::string, shared_ptr<inode>>::iterator it;
+	it = this->child_inodes.find(filename);
+
+	if(it == this->child_inodes.end()) {
+		global_logger.log(indexing_ops, "Non-existing file is tried to deleted");
+		return -1;
+	}
+
+	this->child_inodes.erase(it);
+	return 0;
 }
 
 shared_ptr<inode> dentry_table::get_child_inode(std::string filename){
@@ -34,10 +52,32 @@ shared_ptr<inode> dentry_table::get_child_inode(std::string filename){
 
 }
 
-int dentry_table::add_inode(std::string filename, shared_ptr<inode> inode){
+int dentry_table::pull_child_metadata() {
+	this->dentries = std::make_shared<dentry>(this->dir_ino);
 
+	std::map<std::string, ino_t>::iterator it;
+	for(it = this->dentries->child_list.begin(); it != this->dentries->child_list.end(); it++) {
+		this->add_inode(it->first, std::make_shared<inode>(it->second));
+	}
+
+	return 0;
 }
 
-int pull_child_metadata(ino_t dir_ino){
 
+shared_ptr<inode> dentry_table::get_dir_inode(){
+	if(this->loc == LOCAL) {
+		return this->dir_inode;
+	} else if (this->loc == REMOTE) {
+		/* TODO */
+	}
 }
+
+enum meta_location dentry_table::get_loc() {return this->loc;}
+uint64_t dentry_table::get_leader_id() {return this->leader_id;}
+
+void dentry_table::set_dir_inode(shared_ptr<inode> dir_inode) {this->dir_inode = dir_inode;}
+void dentry_table::set_loc(enum meta_location loc) {this->loc = loc;}
+void dentry_table::set_leader_id(uint64_t leader_id) {this->leader_id = leader_id;}
+
+
+
--- in_memory/dentry_table.hpp
+++ in_memory/dentry_table.hpp
@@ -5,14 +5,13 @@
 #include <utility>
 #include <memory>
 #include "../meta/inode.hpp"
+#include "../meta/dentry.hpp"
 
 enum meta_location {
     LOCAL = 0,
     REMOTE,
     UNKNOWN,
-
-    /* temporaly status */
-    NOBODY
+    NOBODY /* temporaly status */
 };
 
 using std::shared_ptr;
@@ -21,22 +20,31 @@ class dentry_table {
 private:
 	ino_t dir_ino;
 	shared_ptr<inode> dir_inode;
+
+	shared_ptr<dentry> dentries;
 	std::map<std::string, shared_ptr<inode>> child_inodes;
 
-    	enum meta_location loc;
-    	uint64_t leader_client_id;
+	enum meta_location loc;
+	uint64_t leader_id;
 
 public:
-	dentry_table(ino_t dir_ino);
+	explicit dentry_table(ino_t dir_ino, shared_ptr<inode> dir_inode);
 	~dentry_table();
 
-    	int add_inode(std::string filename, shared_ptr<inode> inode);
-    	int delete_inode(std::string filename);
-
-    	shared_ptr<inode> get_dir_inode();
+ 	int add_inode(std::string filename, shared_ptr<inode> inode);
+	int delete_inode(std::string filename);
 	shared_ptr<inode> get_child_inode(std::string filename);
+	int pull_dir_inode_itself();
+	int pull_child_metadata();
+
+	shared_ptr<inode> get_dir_inode();
+	enum meta_location get_loc();
+	uint64_t get_leader_id();
+
+	void set_dir_inode(shared_ptr<inode> dir_inode);
+	void set_loc(enum meta_location loc);
+	void set_leader_id(uint64_t leader_id);
 
 };
 
-int pull_child_metadata(ino_t dir_ino);
 #endif //NMFS0_DENTRY_TABLE_HPP
