7c2f6929c43b5ec6cfed1f213e0e74395a59acee
CHOKYUJIN <bori19960@naver.com>
2021-03-31 16:59:07 +0900

    Fix become_leader() to init dir_inode itself

--- lease/lease_util.cpp
+++ lease/lease_util.cpp
@@ -1,15 +1,14 @@
 #include "lease_util.hpp"
 
-shared_ptr<dentry_table> become_leader(ino_t directory_ino) {
-	shared_ptr<dentry_table> new_dentry_table = std::make_shared<dentry_table>(directory_ino);
+shared_ptr<dentry_table> become_leader(ino_t directory_ino, shared_ptr<inode> dir_inode) {
+	shared_ptr<dentry_table> new_dentry_table = std::make_shared<dentry_table>(directory_ino, dir_inode);
+	new_dentry_table->pull_child_metadata();
 
 	new_dentry_table->set_loc(LOCAL);
-
 	fuse_context *fuse_ctx = fuse_get_context();
 	client *c = (client *) (fuse_ctx->private_data);
 	new_dentry_table->set_leader_id(c->get_client_id());
 
-	new_dentry_table->pull_child_metadata();
 	return new_dentry_table;
 }
 
--- lease/lease_util.hpp
+++ lease/lease_util.hpp
@@ -5,6 +5,6 @@
 #include "../in_memory/dentry_table.hpp"
 #include "../client/client.hpp"
 
-shared_ptr<dentry_table> become_leader(ino_t directory_ino);
+shared_ptr<dentry_table> become_leader(ino_t directory_ino, shared_ptr<inode> dir_inode);
 int retire_leader(ino_t directory_ino);
 #endif //NMFS0_LEASE_UTIL_HPP
