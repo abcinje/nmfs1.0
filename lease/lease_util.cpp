ce1c6c033104c5d2baf6cbb9962a5d1c13612b5a
CHOKYUJIN <bori19960@naver.com>
2021-04-01 18:07:08 +0900

    Revise function parameters

--- lease/lease_util.cpp
+++ lease/lease_util.cpp
@@ -1,13 +1,26 @@
 #include "lease_util.hpp"
 
-shared_ptr<dentry_table> become_leader(ino_t directory_ino, shared_ptr<inode> dir_inode) {
-	shared_ptr<dentry_table> new_dentry_table = std::make_shared<dentry_table>(directory_ino, dir_inode);
+shared_ptr<dentry_table> become_leader(shared_ptr<inode> dir_inode) {
+	shared_ptr<dentry_table> new_dentry_table = std::make_shared<dentry_table>(dir_inode->get_ino(), dir_inode);
 	new_dentry_table->pull_child_metadata();
 
 	new_dentry_table->set_loc(LOCAL);
 	fuse_context *fuse_ctx = fuse_get_context();
 	client *c = (client *) (fuse_ctx->private_data);
 	new_dentry_table->set_leader_id(c->get_client_id());
+
+	return new_dentry_table;
+}
+
+shared_ptr<dentry_table> become_leader_of_new_dir(shared_ptr<inode> parent_inode, shared_ptr<inode> dir_inode){
+	shared_ptr<dentry_table> new_dentry_table = std::make_shared<dentry_table>(dir_inode->get_ino(), dir_inode);
+
+	shared_ptr<dentry> new_d = std::make_shared<dentry>(dir_inode->get_ino(), true);
+	new_d->add_new_child(".", dir_inode->get_ino());
+	new_d->add_new_child("..", parent_inode->get_ino());
+
+	new_dentry_table->set_dentries(new_d);
+	new_d->sync();
 
 	return new_dentry_table;
 }
--- lease/lease_util.hpp
+++ lease/lease_util.hpp
@@ -5,6 +5,7 @@
 #include "../in_memory/dentry_table.hpp"
 #include "../client/client.hpp"
 
-shared_ptr<dentry_table> become_leader(ino_t directory_ino, shared_ptr<inode> dir_inode);
+shared_ptr<dentry_table> become_leader(shared_ptr<inode> dir_inode);
+shared_ptr<dentry_table> become_leader_of_new_dir(shared_ptr<inode> parent_inode, shared_ptr<inode> dir_inode);
 int retire_leader(ino_t directory_ino);
 #endif //NMFS0_LEASE_UTIL_HPP
