9cd64b6496c865f00297a6069a683e957e0c5490
CHOKYUJIN <bori19960@snu.ac.kr>
2021-04-07 17:57:53 +0900

    Revise directory table (root_inode, remove symlink resolve)

--- in_memory/directory_table.cpp
+++ in_memory/directory_table.cpp
@@ -1,4 +1,9 @@
 #include "directory_table.hpp"
+
+directory_table::directory_table() {
+	this->root_inode  = std::make_shared<inode>(0);
+}
+
 directory_table::~directory_table() {
 	global_logger.log(directory_table_ops, "Called ~directory_table()");
 
@@ -12,7 +17,7 @@ shared_ptr<inode> directory_table::path_traversal(std::string path) {
 	global_logger.log(directory_table_ops, "Called path_traverse(" + path + ")");
 
 	shared_ptr<dentry_table> parent_dentry_table = get_dentry_table(0);
-	shared_ptr<inode> parent_inode = parent_dentry_table->get_dir_inode();
+	shared_ptr<inode> parent_inode = this->get_root_inode();
 
 	ino_t check_target_ino;
 	shared_ptr<inode> target_inode;
@@ -33,21 +38,6 @@ shared_ptr<inode> directory_table::path_traversal(std::string path) {
 			throw inode::no_entry("No such file or Directory: in path traversal");
 		else
 			target_inode = parent_dentry_table->get_child_inode(target_name);
-
-		if(S_ISLNK(target_inode->get_mode())) {
-			path = std::string(target_inode->get_link_target_name());
-			if(path.at(0) == '/')
-				end_name = -1;
-			else
-				end_name = -2;
-			path_len = path.length();
-
-			if(set_name_bound(start_name, end_name, path, path_len) == -1)
-				throw std::runtime_error("Link Resolution Error");
-
-			std::string origin_name = path.substr(start_name, end_name - start_name + 1);
-			target_inode = parent_dentry_table->get_child_inode(origin_name);
-		}
 
 		if(S_ISDIR(target_inode->get_mode()))
 			permission_check(target_inode.get(), X_OK);
@@ -74,7 +64,7 @@ shared_ptr<dentry_table> directory_table::get_dentry_table(ino_t ino){
 		global_logger.log(directory_table_ops, "dentry_table : MISS");
 		shared_ptr<inode> i = std::make_shared<inode>(ino);
 		if(i->get_leader_id() == 0) { /* NOBODY -> LOCAL */
-			shared_ptr<dentry_table> new_dentry_table = become_leader(i);
+			shared_ptr<dentry_table> new_dentry_table = become_leader(i->get_ino());
 			this->add_dentry_table(ino, new_dentry_table);
 
 			return new_dentry_table;
@@ -111,4 +101,8 @@ int directory_table::delete_dentry_table(ino_t ino){
 	this->dentry_tables.erase(it);
 
 	return 0;
+}
+
+shared_ptr<inode> directory_table::get_root_inode() {
+	return this->root_inode;
 }
--- in_memory/directory_table.hpp
+++ in_memory/directory_table.hpp
@@ -15,16 +15,19 @@ using std::shared_ptr;
 
 class directory_table {
 private:
+    	shared_ptr<inode> root_inode;
 	std::map<ino_t, shared_ptr<dentry_table>> dentry_tables;
 
 public:
-		~directory_table();
+    	directory_table();
+	~directory_table();
     	int add_dentry_table(ino_t ino, shared_ptr<dentry_table> dtable);
     	int delete_dentry_table(ino_t ino);
 
     	shared_ptr<inode> path_traversal(std::string path);
     	shared_ptr<dentry_table> get_dentry_table(ino_t ino);
 
+    	shared_ptr<inode> get_root_inode();
 };
 
 
