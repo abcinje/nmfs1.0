5bc6f7b07fb6d01aee43500dee5aa173b30da5f5
Injae Kang <abcinje@gmail.com>
2021-04-18 18:41:21 +0900

    Fix minor

--- client/util.cpp
+++ client/util.cpp
@@ -2,37 +2,31 @@
 #include "util.hpp"
 
 std::unique_ptr<std::string> get_parent_dir_path(std::string_view path) {
-	if (path.empty()) {
+	if (path.empty())
 		throw std::runtime_error("Invalid path: path is empty string");
-	} else {
-		for (size_t i = path.length() - 1; i > 0; i--) {
-			if (path[i] == path_delimiter) {
-				return std::move(std::make_unique<std::string>(path.substr(0, i)));
-			}
-		}
-		// root directory
-		if (path[0] == path_delimiter) {
-			return std::move(std::make_unique<std::string>(path.substr(0, 1)));
-		}
 
-		throw std::runtime_error("Invalid path: No delimiter on path");
-	}
+	for (size_t i = path.length() - 1; i > 0; i--)
+		if (path[i] == path_delimiter)
+			return std::move(std::make_unique<std::string>(path.substr(0, i)));
+
+	// root directory
+	if (path[0] == path_delimiter)
+		return std::move(std::make_unique<std::string>(path.substr(0, 1)));
+
+	throw std::runtime_error("Invalid path: No delimiter on path");
 }
 
 std::unique_ptr<std::string> get_filename_from_path(std::string_view path) {
-	if (path.empty()) {
+	if (path.empty())
 		throw std::runtime_error("Invalid path: path is empty string");
-	} else {
-		// root directory
-		if ((path.length() == 1) && (path[0] == path_delimiter)) {
-			return std::move(std::make_unique<std::string>(path.substr(0, 1))); // root directory
-		}
 
-		for (size_t i = path.length() - 1; i >= 0; i--) {
-			if (path[i] == path_delimiter) {
-				return std::move(std::make_unique<std::string>(path.substr(i+1)));
-			}
-		}
-		throw std::runtime_error("Invalid path: No delimiter on path");
-	}
+	// root directory
+	if ((path.length() == 1) && (path[0] == path_delimiter))
+		return std::move(std::make_unique<std::string>(path.substr(0, 1)));
+
+	for (size_t i = path.length() - 1; i >= 0; i--)
+		if (path[i] == path_delimiter)
+			return std::move(std::make_unique<std::string>(path.substr(i+1)));
+
+	throw std::runtime_error("Invalid path: No delimiter on path");
 }
