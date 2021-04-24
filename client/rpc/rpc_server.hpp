1e27538272c0a46ff0e92f76a9d4b2df414d8624
CHOKYUJIN <bori19960@snu.ac.kr>
2021-04-25 00:56:38 +0900

    Initialize rpc_client and rpc_server

---
+++ client/rpc/rpc_server.hpp
@@ -0,0 +1,16 @@
+#ifndef NMFS_RPC_SERVER_HPP
+#define NMFS_RPC_SERVER_HPP
+
+#include <rpc.grpc.pb.h>
+
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::Status;
+
+class rpc_server {
+
+};
+
+
+#endif //NMFS_RPC_SERVER_HPP
