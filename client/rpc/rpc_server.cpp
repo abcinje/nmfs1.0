e5c92730355062aef45624e9602a790684efe7dd
CHOKYUJIN <bori19960@snu.ac.kr>
2021-05-04 02:13:31 +0900

    Complete first version of rpc_server except functions that need 2pc

--- client/rpc/rpc_server.cpp
+++ client/rpc/rpc_server.cpp
@@ -57,7 +57,7 @@ Status rpc_server::rpc_access(::grpc::ServerContext *context, const ::rpc_common
 	return Status::OK;
 }
 
-Status rpc_server::rpc_opendir(::grpc::ServerContext *context, const ::rpc_common_request *request,
+Status rpc_server::rpc_opendir(::grpc::ServerContext *context, const ::rpc_open_opendir_request *request,
 							   ::rpc_common_respond *response) {
 	if (indexing_table->check_dentry_table(request->dentry_table_ino()) != LOCAL) {
 		response->set_ret(-ENOTLEADER);
@@ -82,14 +82,23 @@ Status rpc_server::rpc_readdir(::grpc::ServerContext *context, const ::rpc_commo
 	return Service::rpc_readdir(context, request, writer);
 }
 
-Status rpc_server::rpc_mkdir(::grpc::ServerContext *context, const ::rpc_common_request *request,
+Status rpc_server::rpc_mkdir(::grpc::ServerContext *context, const ::rpc_mkdir_request *request,
 							 ::rpc_common_respond *response) {
 	if (indexing_table->check_dentry_table(request->dentry_table_ino()) != LOCAL) {
 		response->set_ret(-ENOTLEADER);
 		return Status::OK;
 	}
 
-	return Service::rpc_mkdir(context, request, response);
+	fuse_context *fuse_ctx = fuse_get_context();
+	std::shared_ptr<dentry_table> parent_dentry_table = indexing_table->get_dentry_table(request->dentry_table_ino());
+	shared_ptr<inode> i = std::make_shared<inode>(fuse_ctx->uid, fuse_ctx->gid, request->new_mode() | S_IFDIR);
+	parent_dentry_table->create_child_inode(request->new_dir_name(), i);
+
+	i->set_size(DIR_INODE_SIZE);
+	i->sync();
+
+	response->set_ret(0);
+	return Status::OK;
 }
 
 Status rpc_server::rpc_rmdir(::grpc::ServerContext *context, const ::rpc_common_request *request,
@@ -102,7 +111,7 @@ Status rpc_server::rpc_rmdir(::grpc::ServerContext *context, const ::rpc_common_
 	return Service::rpc_rmdir(context, request, response);
 }
 
-Status rpc_server::rpc_symlink(::grpc::ServerContext *context, const ::rpc_common_request *request,
+Status rpc_server::rpc_symlink(::grpc::ServerContext *context, const ::rpc_symlink_request *request,
 							   ::rpc_common_respond *response) {
 	if (indexing_table->check_dentry_table(request->dentry_table_ino()) != LOCAL) {
 		response->set_ret(-ENOTLEADER);
@@ -126,16 +135,11 @@ Status rpc_server::rpc_symlink(::grpc::ServerContext *context, const ::rpc_commo
 	dst_parent_dentry_table->create_child_inode(symlink_name->data(), symlink_i);
 	symlink_i->sync();
 
-	/* TODO : should we change directory inode's size?
-	dst_parent_i->set_size(dst_parent_dentry_table->get_total_name_legth());
-	dst_parent_i->sync();
-	*/
-
 	response->set_ret(0);
 	return Status::OK;
 }
 
-Status rpc_server::rpc_readlink(::grpc::ServerContext *context, const ::rpc_common_request *request,
+Status rpc_server::rpc_readlink(::grpc::ServerContext *context, const ::rpc_readlink_request *request,
 								::rpc_name_respond *response) {
 	if (indexing_table->check_dentry_table(request->dentry_table_ino()) != LOCAL) {
 		response->set_ret(-ENOTLEADER);
@@ -154,14 +158,36 @@ Status rpc_server::rpc_readlink(::grpc::ServerContext *context, const ::rpc_comm
 	return Status::OK;
 }
 
-Status rpc_server::rpc_rename_same_parent(::grpc::ServerContext *context, const ::rpc_common_request *request,
+Status rpc_server::rpc_rename_same_parent(::grpc::ServerContext *context, const ::rpc_rename_same_parent_request *request,
 										  ::rpc_common_respond *response) {
 	if (indexing_table->check_dentry_table(request->dentry_table_ino()) != LOCAL) {
 		response->set_ret(-ENOTLEADER);
 		return Status::OK;
 	}
 
-	return Service::rpc_rename_same_parent(context, request, response);
+	unique_ptr<std::string> old_name = get_filename_from_path(request->old_path());
+	unique_ptr<std::string> new_name = get_filename_from_path(request->new_path());
+
+	std::shared_ptr<dentry_table> parent_dentry_table = indexing_table->get_dentry_table(request->dentry_table_ino());
+
+	shared_ptr<inode> target_i = parent_dentry_table->get_child_inode(*old_name);
+	ino_t check_dst_ino = parent_dentry_table->check_child_inode(*new_name);
+
+	if (request->flags() == 0) {
+		if(check_dst_ino != -1) {
+			parent_dentry_table->delete_child_inode(*new_name);
+			meta_pool->remove(INODE, std::to_string(check_dst_ino));
+		}
+		parent_dentry_table->delete_child_inode(*old_name);
+		parent_dentry_table->create_child_inode(*new_name, target_i);
+
+	} else {
+		response->set_ret(-ENOSYS);
+		return Status::OK;
+	}
+
+	response->set_ret(0);
+	return Status::OK;
 }
 
 Status rpc_server::rpc_rename_not_same_parent(::grpc::ServerContext *context, const ::rpc_common_request *request,
@@ -174,7 +200,7 @@ Status rpc_server::rpc_rename_not_same_parent(::grpc::ServerContext *context, co
 	return Service::rpc_rename_not_same_parent(context, request, response);
 }
 
-Status rpc_server::rpc_open(::grpc::ServerContext *context, const ::rpc_common_request *request,
+Status rpc_server::rpc_open(::grpc::ServerContext *context, const ::rpc_open_opendir_request *request,
 							::rpc_common_respond *response) {
 	if (indexing_table->check_dentry_table(request->dentry_table_ino()) != LOCAL) {
 		response->set_ret(-ENOTLEADER);
@@ -203,82 +229,178 @@ Status rpc_server::rpc_open(::grpc::ServerContext *context, const ::rpc_common_r
 	return Status::OK;
 }
 
-Status rpc_server::rpc_create(::grpc::ServerContext *context, const ::rpc_common_request *request,
+Status rpc_server::rpc_create(::grpc::ServerContext *context, const ::rpc_create_request *request,
+							  ::rpc_create_respond *response) {
+	if (indexing_table->check_dentry_table(request->dentry_table_ino()) != LOCAL) {
+		response->set_ret(-ENOTLEADER);
+		return Status::OK;
+	}
+	fuse_context *fuse_ctx = fuse_get_context();
+	std::shared_ptr<dentry_table> parent_dentry_table = indexing_table->get_dentry_table(request->dentry_table_ino());
+	shared_ptr<inode> i = std::make_shared<inode>(fuse_ctx->uid, fuse_ctx->gid, request->new_mode() | S_IFREG);
+
+	i->sync();
+
+	parent_dentry_table->create_child_inode(request->new_file_name(), i);
+
+	response->set_new_ino(i->get_ino());
+	response->set_ret(0);
+	return Status::OK;
+}
+
+Status rpc_server::rpc_unlink(::grpc::ServerContext *context, const ::rpc_unlink_request *request,
 							  ::rpc_common_respond *response) {
 	if (indexing_table->check_dentry_table(request->dentry_table_ino()) != LOCAL) {
 		response->set_ret(-ENOTLEADER);
 		return Status::OK;
 	}
 
-	return Service::rpc_create(context, request, response);
+	std::shared_ptr<dentry_table> parent_dentry_table = indexing_table->get_dentry_table(request->dentry_table_ino());
+	std::shared_ptr<inode> target_i = parent_dentry_table->get_child_inode(request->filename());
+
+	nlink_t nlink = target_i->get_nlink() - 1;
+	if (nlink == 0) {
+		/* data */
+		data_pool->remove(DATA, std::to_string(target_i->get_ino()));
+
+		/* inode */
+		meta_pool->remove(INODE, std::to_string(target_i->get_ino()));
+
+		/* parent dentry */
+		parent_dentry_table->delete_child_inode(request->filename());
+	} else {
+		target_i->set_nlink(nlink);
+		target_i->sync();
+	}
+
+	response->set_ret(0);
+	return Status::OK;
 }
 
-Status rpc_server::rpc_unlink(::grpc::ServerContext *context, const ::rpc_common_request *request,
-							  ::rpc_common_respond *response) {
+Status rpc_server::rpc_write(::grpc::ServerContext *context, const ::rpc_write_request *request,
+							 ::rpc_write_respond *response) {
 	if (indexing_table->check_dentry_table(request->dentry_table_ino()) != LOCAL) {
 		response->set_ret(-ENOTLEADER);
 		return Status::OK;
 	}
 
-	return Service::rpc_unlink(context, request, response);
-}
+	std::shared_ptr<dentry_table> parent_dentry_table = indexing_table->get_dentry_table(request->dentry_table_ino());
+	std::shared_ptr<inode> i = parent_dentry_table->get_child_inode(request->filename());
 
-Status rpc_server::rpc_read(::grpc::ServerContext *context, const ::rpc_common_request *request,
-							::rpc_common_respond *response) {
-	if (indexing_table->check_dentry_table(request->dentry_table_ino()) != LOCAL) {
-		response->set_ret(-ENOTLEADER);
-		return Status::OK;
+	off_t offset = request->offset();
+	size_t size = request->size();
+
+	if(request->flags() & O_APPEND) {
+		offset = i->get_size();
 	}
 
-	return Service::rpc_read(context, request, response);
+	if (i->get_size() < offset + size) {
+		i->set_size(offset + size);
+		i->sync();
+	}
+
+	response->set_offset(offset);
+	response->set_size(size);
+	response->set_ret(0);
+	return Status::OK;
 }
 
-Status rpc_server::rpc_write(::grpc::ServerContext *context, const ::rpc_common_request *request,
+Status rpc_server::rpc_chmod(::grpc::ServerContext *context, const ::rpc_chmod_request *request,
 							 ::rpc_common_respond *response) {
 	if (indexing_table->check_dentry_table(request->dentry_table_ino()) != LOCAL) {
 		response->set_ret(-ENOTLEADER);
 		return Status::OK;
 	}
 
-	return Service::rpc_write(context, request, response);
+	std::shared_ptr<dentry_table> parent_dentry_table = indexing_table->get_dentry_table(request->dentry_table_ino());
+	std::shared_ptr<inode> i = parent_dentry_table->get_child_inode(request->filename());
+
+	mode_t type = i->get_mode() & S_IFMT;
+
+	i->set_mode(request->mode() | type);
+	i->sync();
+
+	response->set_ret(0);
+	return Status::OK;
 }
 
-Status rpc_server::rpc_chmod(::grpc::ServerContext *context, const ::rpc_common_request *request,
+Status rpc_server::rpc_chown(::grpc::ServerContext *context, const ::rpc_chown_request *request,
 							 ::rpc_common_respond *response) {
 	if (indexing_table->check_dentry_table(request->dentry_table_ino()) != LOCAL) {
 		response->set_ret(-ENOTLEADER);
 		return Status::OK;
 	}
 
-	return Service::rpc_chmod(context, request, response);
+	std::shared_ptr<dentry_table> parent_dentry_table = indexing_table->get_dentry_table(request->dentry_table_ino());
+	std::shared_ptr<inode> i = parent_dentry_table->get_child_inode(request->filename());
+
+	if (((int32_t) request->uid()) >= 0)
+		i->set_uid(request->uid());
+
+	if (((int32_t) request->gid()) >= 0)
+		i->set_gid(request->gid());
+
+	i->sync();
+
+	response->set_ret(0);
+	return Status::OK;
 }
 
-Status rpc_server::rpc_chown(::grpc::ServerContext *context, const ::rpc_common_request *request,
-							 ::rpc_common_respond *response) {
-	if (indexing_table->check_dentry_table(request->dentry_table_ino()) != LOCAL) {
-		response->set_ret(-ENOTLEADER);
-		return Status::OK;
-	}
-
-	return Service::rpc_chown(context, request, response);
-}
-
-Status rpc_server::rpc_utimens(::grpc::ServerContext *context, const ::rpc_common_request *request,
+Status rpc_server::rpc_utimens(::grpc::ServerContext *context, const ::rpc_utimens_request *request,
 							   ::rpc_common_respond *response) {
 	if (indexing_table->check_dentry_table(request->dentry_table_ino()) != LOCAL) {
 		response->set_ret(-ENOTLEADER);
 		return Status::OK;
 	}
 
-	return Service::rpc_utimens(context, request, response);
+	std::shared_ptr<dentry_table> parent_dentry_table = indexing_table->get_dentry_table(request->dentry_table_ino());
+	std::shared_ptr<inode> i = parent_dentry_table->get_child_inode(request->filename());
+
+	if (request->a_nsec() == UTIME_NOW) {
+		struct timespec ts;
+		if (!timespec_get(&ts, TIME_UTC))
+			runtime_error("timespec_get() failed");
+		i->set_atime(ts);
+	} else if (request->a_nsec() == UTIME_OMIT) { ;
+	} else {
+		struct timespec tv;
+		tv.tv_sec = request->a_sec();
+		tv.tv_nsec = request->a_nsec();
+		i->set_atime(tv);
+	}
+
+	if (request->m_nsec() == UTIME_NOW) {
+		struct timespec ts;
+		if (!timespec_get(&ts, TIME_UTC))
+			runtime_error("timespec_get() failed");
+		i->set_mtime(ts);
+	} else if (request->m_nsec()  == UTIME_OMIT) { ;
+	} else {
+		struct timespec tv;
+		tv.tv_sec = request->m_sec();
+		tv.tv_nsec = request->m_nsec();
+		i->set_mtime(tv);
+	}
+
+	i->sync();
+
+	response->set_ret(0);
+	return Status::OK;
 }
 
-Status rpc_server::rpc_truncate(::grpc::ServerContext *context, const ::rpc_common_request *request,
+Status rpc_server::rpc_truncate(::grpc::ServerContext *context, const ::rpc_truncate_request *request,
 								::rpc_common_respond *response) {
 	if (indexing_table->check_dentry_table(request->dentry_table_ino()) != LOCAL) {
 		response->set_ret(-ENOTLEADER);
 		return Status::OK;
 	}
 
-	return Service::rpc_truncate(context, request, response);
+	std::shared_ptr<dentry_table> parent_dentry_table = indexing_table->get_dentry_table(request->dentry_table_ino());
+	std::shared_ptr<inode> i = parent_dentry_table->get_child_inode(request->filename());
+
+	i->set_size(request->offset());
+	i->sync();
+
+	response->set_ret(0);
+	return Status::OK;
 }
--- client/rpc/rpc_server.hpp
+++ client/rpc/rpc_server.hpp
@@ -13,62 +13,59 @@ using grpc::Status;
 
 class rpc_server : public remote_ops::Service {
 public:
-	Status rpc_getattr(::grpc::ServerContext *context, const ::rpc_common_request *request,
-					   ::rpc_getattr_respond *response) override;
+    Status rpc_getattr(::grpc::ServerContext *context, const ::rpc_common_request *request,
+		       ::rpc_getattr_respond *response) override;
 
-	Status rpc_access(::grpc::ServerContext *context, const ::rpc_common_request *request,
-					  ::rpc_common_respond *response) override;
+    Status rpc_access(::grpc::ServerContext *context, const ::rpc_common_request *request,
+		      ::rpc_common_respond *response) override;
 
-	Status rpc_opendir(::grpc::ServerContext *context, const ::rpc_common_request *request,
-					   ::rpc_common_respond *response) override;
+    Status rpc_opendir(::grpc::ServerContext *context, const ::rpc_open_opendir_request *request,
+		       ::rpc_common_respond *response) override;
 
-	Status rpc_readdir(::grpc::ServerContext *context, const ::rpc_common_request *request,
-					   ::grpc::ServerWriter<::rpc_name_respond> *writer) override;
+    Status rpc_readdir(::grpc::ServerContext *context, const ::rpc_common_request *request,
+		       ::grpc::ServerWriter<::rpc_name_respond> *writer) override;
 
-	Status rpc_mkdir(::grpc::ServerContext *context, const ::rpc_common_request *request,
-					 ::rpc_common_respond *response) override;
+    Status rpc_mkdir(::grpc::ServerContext *context, const ::rpc_mkdir_request *request,
+		     ::rpc_common_respond *response) override;
 
-	Status rpc_rmdir(::grpc::ServerContext *context, const ::rpc_common_request *request,
-					 ::rpc_common_respond *response) override;
+    Status rpc_rmdir(::grpc::ServerContext *context, const ::rpc_common_request *request,
+		     ::rpc_common_respond *response) override;
 
-	Status rpc_symlink(::grpc::ServerContext *context, const ::rpc_common_request *request,
-					   ::rpc_common_respond *response) override;
+    Status rpc_symlink(::grpc::ServerContext *context, const ::rpc_symlink_request *request,
+		       ::rpc_common_respond *response) override;
 
-	Status rpc_readlink(::grpc::ServerContext *context, const ::rpc_common_request *request,
-						::rpc_name_respond *response) override;
+    Status rpc_readlink(::grpc::ServerContext *context, const ::rpc_readlink_request *request,
+			::rpc_name_respond *response) override;
 
-	Status rpc_rename_same_parent(::grpc::ServerContext *context, const ::rpc_common_request *request,
-								  ::rpc_common_respond *response) override;
+    Status rpc_rename_same_parent(::grpc::ServerContext *context, const ::rpc_rename_same_parent_request *request,
+				  ::rpc_common_respond *response) override;
 
-	Status rpc_rename_not_same_parent(::grpc::ServerContext *context, const ::rpc_common_request *request,
-									  ::rpc_common_respond *response) override;
+    Status rpc_rename_not_same_parent(::grpc::ServerContext *context, const ::rpc_common_request *request,
+				      ::rpc_common_respond *response) override;
 
-	Status rpc_open(::grpc::ServerContext *context, const ::rpc_common_request *request,
-					::rpc_common_respond *response) override;
+    Status rpc_open(::grpc::ServerContext *context, const ::rpc_open_opendir_request *request,
+		    ::rpc_common_respond *response) override;
 
-	Status rpc_create(::grpc::ServerContext *context, const ::rpc_common_request *request,
-					  ::rpc_common_respond *response) override;
+    Status rpc_create(::grpc::ServerContext *context, const ::rpc_create_request *request,
+		      ::rpc_create_respond *response) override;
 
-	Status rpc_unlink(::grpc::ServerContext *context, const ::rpc_common_request *request,
-					  ::rpc_common_respond *response) override;
+    Status rpc_unlink(::grpc::ServerContext *context, const ::rpc_unlink_request *request,
+		      ::rpc_common_respond *response) override;
 
-	Status rpc_read(::grpc::ServerContext *context, const ::rpc_common_request *request,
-					::rpc_common_respond *response) override;
+    Status rpc_write(::grpc::ServerContext *context, const ::rpc_write_request *request,
+		     ::rpc_write_respond *response) override;
 
-	Status rpc_write(::grpc::ServerContext *context, const ::rpc_common_request *request,
-					 ::rpc_common_respond *response) override;
+    Status rpc_chmod(::grpc::ServerContext *context, const ::rpc_chmod_request *request,
+		     ::rpc_common_respond *response) override;
 
-	Status rpc_chmod(::grpc::ServerContext *context, const ::rpc_common_request *request,
-					 ::rpc_common_respond *response) override;
+    Status rpc_chown(::grpc::ServerContext *context, const ::rpc_chown_request *request,
+		     ::rpc_common_respond *response) override;
 
-	Status rpc_chown(::grpc::ServerContext *context, const ::rpc_common_request *request,
-					 ::rpc_common_respond *response) override;
+    Status rpc_utimens(::grpc::ServerContext *context, const ::rpc_utimens_request *request,
+		       ::rpc_common_respond *response) override;
 
-	Status rpc_utimens(::grpc::ServerContext *context, const ::rpc_common_request *request,
-					   ::rpc_common_respond *response) override;
-
-	Status rpc_truncate(::grpc::ServerContext *context, const ::rpc_common_request *request,
-						::rpc_common_respond *response) override;
+    Status rpc_truncate(::grpc::ServerContext *context, const ::rpc_truncate_request *request,
+			::rpc_common_respond *response) override;
 
 };
 
