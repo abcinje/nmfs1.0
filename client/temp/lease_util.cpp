37f1deaf612f76ee8517d8f925872f2ed0be3ffd
CHOKYUJIN <bori19960@naver.com>
2021-05-05 18:14:10 +0900

    Remove unnecessary code in temp lease functions

--- client/temp/lease_util.cpp
+++ client/temp/lease_util.cpp
@@ -5,20 +5,12 @@ shared_ptr<dentry_table> become_leader(ino_t ino) {
 	shared_ptr<dentry_table> new_dentry_table = std::make_shared<dentry_table>(ino, LOCAL);
 	new_dentry_table->pull_child_metadata();
 
-	fuse_context *fuse_ctx = fuse_get_context();
-	client *c = (client *) (fuse_ctx->private_data);
-	new_dentry_table->set_leader_id(c->get_client_id());
-
 	return new_dentry_table;
 }
 
 shared_ptr<dentry_table> become_leader_of_new_dir(ino_t parnet_ino, ino_t ino){
 	global_logger.log(lease_ops, "become_leader_of_new_dir(" + std::to_string(ino) + ")");
 	shared_ptr<dentry_table> new_dentry_table = std::make_shared<dentry_table>(ino, LOCAL);
-
-	fuse_context *fuse_ctx = fuse_get_context();
-	client *c = (client *) (fuse_ctx->private_data);
-	new_dentry_table->set_leader_id(c->get_client_id());
 
 	shared_ptr<dentry> new_d = std::make_shared<dentry>(ino, true);
 
