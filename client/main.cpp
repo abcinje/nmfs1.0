128c8425ed43f10f740d162a9daee58f8628a180
CHOKYUJIN <bori19960@naver.com>
2021-05-06 18:27:37 +0900

    Fix main to receive IP:PORT arg

--- client/main.cpp
+++ client/main.cpp
@@ -2,8 +2,8 @@
 
 int main(int argc, char *argv[])
 {
+	/* ./nmfs ARGS MOUNT_POINT "MANAGE_IP:PORT,REMOTE_IP:PORT" */
 	fuse_operations fops = fuse_ops::get_fuse_ops();
-	fuse_main(argc, argv, &fops, NULL);
-
+	fuse_main(argc-1, argv, &fops, argv[argc-1]);
 	return 0;
 }
