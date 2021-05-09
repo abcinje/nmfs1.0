a03932ce9dc84dd26ab315c410b8e3627a083a83
CHOKYUJIN <bori19960@snu.ac.kr>
2021-05-10 02:28:26 +0900

    Add hexdump function

--- client/util.hpp
+++ client/util.hpp
@@ -5,9 +5,12 @@
 #include <string_view>
 #include <memory>
 
+#define HEXDUMP_COLS 16
+
 constexpr char path_delimiter = '/';
 
 std::unique_ptr<std::string> get_parent_dir_path(std::string_view path);
 std::unique_ptr<std::string> get_filename_from_path(std::string_view path);
+void hexdump(void *mem, unsigned int len);
 
 #endif //NMFS0_UTIL_HPP
