8df70bddcd5ff2e49e52b995ff66e8f1e12de6ea
CHOKYUJIN <bori19960@snu.ac.kr>
2021-05-13 16:59:05 +0900

    Add rc_list to reuse rpc_client

--- client/fs_ops/remote_ops.cpp
+++ client/fs_ops/remote_ops.cpp
@@ -1,9 +1,26 @@
 #include "remote_ops.hpp"
 
+/* <address, channel> */
+std::map<std::string, std::shared_ptr<rpc_client>> rc_list;
+
+std::shared_ptr<rpc_client> get_rpc_client(const std::string& remote_address) {
+	global_logger.log(remote_fs_op, "Called  get_rpc_client()");
+	std::map<std::string, std::shared_ptr<rpc_client>>::iterator it;
+	it = rc_list.find(remote_address);
+
+	if(it != rc_list.end()){
+		return it->second;
+	} else {
+		std::shared_ptr<rpc_client> rc = std::make_shared<rpc_client>(grpc::CreateChannel(remote_address, grpc::InsecureChannelCredentials()));
+		rc_list.insert(std::make_pair(remote_address,rc));
+		return rc;
+	}
+}
+
 void remote_getattr(shared_ptr<remote_inode> i, struct stat* stat) {
 	global_logger.log(remote_fs_op, "Called remote_getattr()");
 	std::string remote_address(i->get_address());
-	std::unique_ptr<rpc_client> rc = std::make_unique<rpc_client>(grpc::CreateChannel(remote_address, grpc::InsecureChannelCredentials()));
+	std::shared_ptr<rpc_client> rc = get_rpc_client(remote_address);
 
 	rc->getattr(i, stat);
 }
@@ -11,7 +28,7 @@ void remote_getattr(shared_ptr<remote_inode> i, struct stat* stat) {
 void remote_access(shared_ptr<remote_inode> i, int mask) {
 	global_logger.log(remote_fs_op, "Called remote_access()");
 	std::string remote_address(i->get_address());
-	std::unique_ptr<rpc_client> rc = std::make_unique<rpc_client>(grpc::CreateChannel(remote_address, grpc::InsecureChannelCredentials()));
+	std::shared_ptr<rpc_client> rc = get_rpc_client(remote_address);
 
 	rc->access(i, mask);
 }
@@ -19,7 +36,7 @@ void remote_access(shared_ptr<remote_inode> i, int mask) {
 int remote_opendir(shared_ptr<remote_inode> i, struct fuse_file_info* file_info) {
 	global_logger.log(remote_fs_op, "Called remote_opendir()");
 	std::string remote_address(i->get_address());
-	std::unique_ptr<rpc_client> rc = std::make_unique<rpc_client>(grpc::CreateChannel(remote_address, grpc::InsecureChannelCredentials()));
+	std::shared_ptr<rpc_client> rc = get_rpc_client(remote_address);
 
 	int ret = rc->opendir(i, file_info);
 	return ret;
@@ -28,7 +45,7 @@ int remote_opendir(shared_ptr<remote_inode> i, struct fuse_file_info* file_info)
 void remote_readdir(shared_ptr<remote_inode> i, void* buffer, fuse_fill_dir_t filler) {
 	global_logger.log(remote_fs_op, "Called remote_readdir()");
 	std::string remote_address(i->get_address());
-	std::unique_ptr<rpc_client> rc = std::make_unique<rpc_client>(grpc::CreateChannel(remote_address, grpc::InsecureChannelCredentials()));
+	std::shared_ptr<rpc_client> rc = get_rpc_client(remote_address);
 
 	rc->readdir(i, buffer, filler);
 }
@@ -36,7 +53,7 @@ void remote_readdir(shared_ptr<remote_inode> i, void* buffer, fuse_fill_dir_t fi
 ino_t remote_mkdir(shared_ptr<remote_inode> parent_i, std::string new_child_name, mode_t mode) {
 	global_logger.log(remote_fs_op, "Called remote_mkdir()");
 	std::string remote_address(parent_i->get_address());
-	std::unique_ptr<rpc_client> rc = std::make_unique<rpc_client>(grpc::CreateChannel(remote_address, grpc::InsecureChannelCredentials()));
+	std::shared_ptr<rpc_client> rc = get_rpc_client(remote_address);
 
 	ino_t new_dir_ino = rc->mkdir(parent_i, new_child_name, mode);
 	return new_dir_ino;
@@ -45,7 +62,7 @@ ino_t remote_mkdir(shared_ptr<remote_inode> parent_i, std::string new_child_name
 int remote_rmdir_top(shared_ptr<remote_inode> target_i, std::string target_name) {
 	global_logger.log(remote_fs_op, "Called remote_rmdir_top()");
 	std::string remote_address(target_i->get_address());
-	std::unique_ptr<rpc_client> rc = std::make_unique<rpc_client>(grpc::CreateChannel(remote_address, grpc::InsecureChannelCredentials()));
+	std::shared_ptr<rpc_client> rc = get_rpc_client(remote_address);
 
 	int ret = rc->rmdir_top(target_i, target_name);
 	return ret;
@@ -54,7 +71,7 @@ int remote_rmdir_top(shared_ptr<remote_inode> target_i, std::string target_name)
 int remote_rmdir_down(shared_ptr<remote_inode> parent_i, ino_t target_ino, std::string target_name) {
 	global_logger.log(remote_fs_op, "Called remote_rmdir_down()");
 	std::string remote_address(parent_i->get_address());
-	std::unique_ptr<rpc_client> rc = std::make_unique<rpc_client>(grpc::CreateChannel(remote_address, grpc::InsecureChannelCredentials()));
+	std::shared_ptr<rpc_client> rc = get_rpc_client(remote_address);
 
 	rc->rmdir_down(parent_i, target_ino, target_name);
 	return 0;
@@ -63,7 +80,7 @@ int remote_rmdir_down(shared_ptr<remote_inode> parent_i, ino_t target_ino, std::
 int remote_symlink(shared_ptr<remote_inode> dst_parent_i, const char *src, const char *dst) {
 	global_logger.log(remote_fs_op, "Called remote_synlink()");
 	std::string remote_address(dst_parent_i->get_address());
-	std::unique_ptr<rpc_client> rc = std::make_unique<rpc_client>(grpc::CreateChannel(remote_address, grpc::InsecureChannelCredentials()));
+	std::shared_ptr<rpc_client> rc = get_rpc_client(remote_address);
 
 	int ret = rc->symlink(dst_parent_i, src, dst);
 	return ret;
@@ -72,7 +89,7 @@ int remote_symlink(shared_ptr<remote_inode> dst_parent_i, const char *src, const
 int remote_readlink(shared_ptr<remote_inode> i, char *buf, size_t size) {
 	global_logger.log(remote_fs_op, "Called remote_readlink()");
 	std::string remote_address(i->get_address());
-	std::unique_ptr<rpc_client> rc = std::make_unique<rpc_client>(grpc::CreateChannel(remote_address, grpc::InsecureChannelCredentials()));
+	std::shared_ptr<rpc_client> rc = get_rpc_client(remote_address);
 
 	int ret = rc->readlink(i, buf, size);
 	return ret;
@@ -81,7 +98,7 @@ int remote_readlink(shared_ptr<remote_inode> i, char *buf, size_t size) {
 int remote_rename_same_parent(shared_ptr<remote_inode> parent_i, const char* old_path, const char* new_path, unsigned int flags) {
 	global_logger.log(remote_fs_op, "Called remote_rename_same_parent()");
 	std::string remote_address(parent_i->get_address());
-	std::unique_ptr<rpc_client> rc = std::make_unique<rpc_client>(grpc::CreateChannel(remote_address, grpc::InsecureChannelCredentials()));
+	std::shared_ptr<rpc_client> rc = get_rpc_client(remote_address);
 
 	int ret = rc->rename_same_parent(parent_i, old_path, new_path, flags);
 	return ret;
@@ -90,7 +107,7 @@ int remote_rename_same_parent(shared_ptr<remote_inode> parent_i, const char* old
 ino_t remote_rename_not_same_parent_src(shared_ptr<remote_inode> src_parent_i, const char* old_path, unsigned int flags) {
 	global_logger.log(remote_fs_op, "Called remote_rename_not_same_parent_src()");
 	std::string remote_address(src_parent_i->get_address());
-	std::unique_ptr<rpc_client> rc = std::make_unique<rpc_client>(grpc::CreateChannel(remote_address, grpc::InsecureChannelCredentials()));
+	std::shared_ptr<rpc_client> rc = get_rpc_client(remote_address);
 
 	ino_t target_ino = rc->rename_not_same_parent_src(src_parent_i, old_path, flags);
 	return target_ino;
@@ -99,7 +116,7 @@ ino_t remote_rename_not_same_parent_src(shared_ptr<remote_inode> src_parent_i, c
 int remote_rename_not_same_parent_dst(shared_ptr<remote_inode> dst_parent_i, ino_t target_ino, ino_t check_dst_ino, const char* new_path, unsigned int flags) {
 	global_logger.log(remote_fs_op, "Called remote_rename_not_same_parent_dst()");
 	std::string remote_address(dst_parent_i->get_address());
-	std::unique_ptr<rpc_client> rc = std::make_unique<rpc_client>(grpc::CreateChannel(remote_address, grpc::InsecureChannelCredentials()));
+	std::shared_ptr<rpc_client> rc = get_rpc_client(remote_address);
 
 	int ret = rc->rename_not_same_parent_dst(dst_parent_i, target_ino, check_dst_ino, new_path, flags);
 	return ret;
@@ -114,7 +131,7 @@ int remote_rename_not_same_parent(shared_ptr<remote_inode> src_parent_i, shared_
 int remote_open(shared_ptr<remote_inode> i, struct fuse_file_info* file_info) {
 	global_logger.log(remote_fs_op, "Called remote_open()");
 	std::string remote_address(i->get_address());
-	std::unique_ptr<rpc_client> rc = std::make_unique<rpc_client>(grpc::CreateChannel(remote_address, grpc::InsecureChannelCredentials()));
+	std::shared_ptr<rpc_client> rc = get_rpc_client(remote_address);
 
 	int ret = rc->open(i, file_info);
 	return ret;
@@ -123,7 +140,7 @@ int remote_open(shared_ptr<remote_inode> i, struct fuse_file_info* file_info) {
 void remote_create(shared_ptr<remote_inode> parent_i, std::string new_child_name, mode_t mode, struct fuse_file_info* file_info) {
 	global_logger.log(remote_fs_op, "Called remote_create()");
 	std::string remote_address(parent_i->get_address());
-	std::unique_ptr<rpc_client> rc = std::make_unique<rpc_client>(grpc::CreateChannel(remote_address, grpc::InsecureChannelCredentials()));
+	std::shared_ptr<rpc_client> rc = get_rpc_client(remote_address);
 
 	rc->create(parent_i, new_child_name, mode, file_info);
 }
@@ -131,7 +148,7 @@ void remote_create(shared_ptr<remote_inode> parent_i, std::string new_child_name
 void remote_unlink(shared_ptr<remote_inode> parent_i, std::string child_name) {
 	global_logger.log(remote_fs_op, "Called remote_unlink()");
 	std::string remote_address(parent_i->get_address());
-	std::unique_ptr<rpc_client> rc = std::make_unique<rpc_client>(grpc::CreateChannel(remote_address, grpc::InsecureChannelCredentials()));
+	std::shared_ptr<rpc_client> rc = get_rpc_client(remote_address);
 
 	rc->unlink(parent_i, child_name);
 }
@@ -139,7 +156,7 @@ void remote_unlink(shared_ptr<remote_inode> parent_i, std::string child_name) {
 size_t remote_write(shared_ptr<remote_inode> i, const char* buffer, size_t size, off_t offset, int flags) {
 	global_logger.log(remote_fs_op, "Called remote_write()");
 	std::string remote_address(i->get_address());
-	std::unique_ptr<rpc_client> rc = std::make_unique<rpc_client>(grpc::CreateChannel(remote_address, grpc::InsecureChannelCredentials()));
+	std::shared_ptr<rpc_client> rc = get_rpc_client(remote_address);
 
 	size_t written_len = rc->write(i, buffer, size, offset, flags);
 	return written_len;
@@ -148,7 +165,7 @@ size_t remote_write(shared_ptr<remote_inode> i, const char* buffer, size_t size,
 void remote_chmod(shared_ptr<remote_inode> i, mode_t mode) {
 	global_logger.log(remote_fs_op, "Called remote_chmod()");
 	std::string remote_address(i->get_address());
-	std::unique_ptr<rpc_client> rc = std::make_unique<rpc_client>(grpc::CreateChannel(remote_address, grpc::InsecureChannelCredentials()));
+	std::shared_ptr<rpc_client> rc = get_rpc_client(remote_address);
 
 	rc->chmod(i, mode);
 }
@@ -156,7 +173,7 @@ void remote_chmod(shared_ptr<remote_inode> i, mode_t mode) {
 void remote_chown(shared_ptr<remote_inode> i, uid_t uid, gid_t gid) {
 	global_logger.log(remote_fs_op, "Called remote_chown()");
 	std::string remote_address(i->get_address());
-	std::unique_ptr<rpc_client> rc = std::make_unique<rpc_client>(grpc::CreateChannel(remote_address, grpc::InsecureChannelCredentials()));
+	std::shared_ptr<rpc_client> rc = get_rpc_client(remote_address);
 
 	rc->chown(i, uid, gid);
 }
@@ -164,7 +181,7 @@ void remote_chown(shared_ptr<remote_inode> i, uid_t uid, gid_t gid) {
 void remote_utimens(shared_ptr<remote_inode> i, const struct timespec tv[2]) {
 	global_logger.log(remote_fs_op, "Called remote_utimens()");
 	std::string remote_address(i->get_address());
-	std::unique_ptr<rpc_client> rc = std::make_unique<rpc_client>(grpc::CreateChannel(remote_address, grpc::InsecureChannelCredentials()));
+	std::shared_ptr<rpc_client> rc = get_rpc_client(remote_address);
 
 	rc->utimens(i, tv);
 }
@@ -172,7 +189,7 @@ void remote_utimens(shared_ptr<remote_inode> i, const struct timespec tv[2]) {
 int remote_truncate (shared_ptr<remote_inode> i, off_t offset) {
 	global_logger.log(remote_fs_op, "Called remote_truncate()");
 	std::string remote_address(i->get_address());
-	std::unique_ptr<rpc_client> rc = std::make_unique<rpc_client>(grpc::CreateChannel(remote_address, grpc::InsecureChannelCredentials()));
+	std::shared_ptr<rpc_client> rc = get_rpc_client(remote_address);
 
 	int ret = rc->truncate(i, offset);
 	return ret;
