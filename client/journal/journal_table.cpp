c7475657e147203a7a98f2eb5f664f108c3ae9c3
Injae Kang <abcinje@gmail.com>
2021-05-17 02:13:31 +0900

    Add journal_table

---
+++ client/journal/journal_table.cpp
@@ -0,0 +1,21 @@
+#include "journal_table.hpp"
+
+std::shared_ptr<transaction> journal_table::get_entry(ino_t ino)
+{
+	{
+		std::shared_lock lock(sm);
+		auto it = map.find(ino);
+		if (it != map.end())
+			return it->second;
+	}
+
+	{
+		std::unique_lock lock(sm);
+		auto ret = map.insert({ino, nullptr});
+		if (ret.second) {
+			return ret.first.value() = std::make_shared<transaction>();
+		} else {
+			return ret.first->second;
+		}
+	}
+}
