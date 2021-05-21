cbb9f969554921c6c2441aabf78b181716a32e42
CHOKYUJIN <bori19960@naver.com>
2021-05-21 18:12:40 +0900

    Fix minor

--- client/meta/remote_inode.hpp
+++ client/meta/remote_inode.hpp
@@ -17,13 +17,13 @@ private :
 	std::string file_name;
 
 public:
-    	remote_inode(std::string leader_ip, uuid dentry_table_ino, std::string file_name, bool target_is_parent = false);
+	remote_inode(std::string leader_ip, uuid dentry_table_ino, std::string file_name, bool target_is_parent = false);
 
 	[[nodiscard]] const string &get_address() const;
 
 	[[nodiscard]] uuid get_dentry_table_ino() const;
 	[[nodiscard]] const string &get_file_name() const;
-    	[[nodiscard]] bool get_target_is_parent() const;
+	[[nodiscard]] bool get_target_is_parent() const;
 	mode_t get_mode() override;
     	void permission_check(int mask) override;
 };
