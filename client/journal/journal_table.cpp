3b1c952cbbd9eb9833dcd86a55c6e0b5ebdc39ac
Injae Kang <abcinje@gmail.com>
2021-05-24 00:19:09 +0900

    Add journal_table::replace_map()

--- client/journal/journal_table.cpp
+++ client/journal/journal_table.cpp
@@ -1,21 +1,35 @@
 #include "journal_table.hpp"
 
+journal_table::journal_table(void)
+{
+	map = std::make_unique<tsl::robin_map<std::string, std::shared_ptr<transaction>>>();
+}
+
 std::shared_ptr<transaction> journal_table::get_entry(uuid ino)
 {
 	{
 		std::shared_lock lock(sm);
-		auto it = map.find(uuid_to_string(ino));
-		if (it != map.end())
+		auto it = map->find(uuid_to_string(ino));
+		if (it != map->end())
 			return it->second;
 	}
 
 	{
 		std::unique_lock lock(sm);
-		auto ret = map.insert({uuid_to_string(ino), nullptr});
+		auto ret = map->insert({uuid_to_string(ino), nullptr});
 		if (ret.second) {
 			return ret.first.value() = std::make_shared<transaction>();
 		} else {
 			return ret.first->second;
 		}
 	}
+}
+
+std::unique_ptr<tsl::robin_map<std::string, std::shared_ptr<transaction>>> journal_table::replace_map(void)
+{
+	std::unique_lock lock(sm);
+
+	auto temp = std::move(map);
+	map = std::make_unique<tsl::robin_map<std::string, std::shared_ptr<transaction>>>();
+	return temp;
 }
--- client/journal/journal_table.hpp
+++ client/journal/journal_table.hpp
@@ -10,10 +10,14 @@
 class journal_table {
 private:
 	std::shared_mutex sm;
-	tsl::robin_map<std::string, std::shared_ptr<transaction>> map;
+	std::unique_ptr<tsl::robin_map<std::string, std::shared_ptr<transaction>>> map;
 
 public:
+	journal_table(void);
+	~journal_table(void) = default;
+
 	std::shared_ptr<transaction> get_entry(uuid ino);
+	std::unique_ptr<tsl::robin_map<std::string, std::shared_ptr<transaction>>> replace_map(void);
 };
 
 #endif /* _JOURNAL_TABLE_HPP_ */
