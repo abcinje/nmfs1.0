7607b8026de88d5017e26abe45dbfbfd1d81bd19
Injae Kang <abcinje@gmail.com>
2021-05-28 15:46:27 +0900

    Add journal core

---
+++ client/journal/journal.cpp
@@ -0,0 +1,59 @@
+#include "journal.hpp"
+
+journal::journal(rados_io *meta_pool, std::shared_ptr<lease_client> lease) : meta(meta_pool), lc(lease), stopped(false)
+{
+	commit_thr = std::make_unique<std::thread>(commit(&stopped, meta, &jtable, &q));
+	checkpoint_thr = std::make_unique<std::thread>(checkpoint(meta, &q));
+}
+
+journal::~journal(void)
+{
+	stopped = true;
+	commit_thr->join();
+	checkpoint_thr->join();
+}
+
+void journal::set_inode(const uuid &self_ino, std::shared_ptr<inode> i)
+{
+	while (true) {
+		auto tx = jtable.get_entry(self_ino);
+		if (!tx->set_inode(i))
+			break;
+	}
+}
+
+void journal::mkdir(const uuid &self_ino, const std::string &d_name, const uuid &d_ino, const struct timespec &time)
+{
+	while (true) {
+		auto tx = jtable.get_entry(self_ino);
+		if (!tx->mkdir(d_name, d_ino, time))
+			break;
+	}
+}
+
+void journal::rmdir(const uuid &self_ino, const std::string &d_name, const uuid &d_ino, const struct timespec &time)
+{
+	while (true) {
+		auto tx = jtable.get_entry(self_ino);
+		if (!tx->rmdir(d_name, d_ino, time))
+			break;
+	}
+}
+
+void journal::mkreg(const uuid &self_ino, const std::string &f_name, std::shared_ptr<inode> f_inode, const struct timespec &time)
+{
+	while (true) {
+		auto tx = jtable.get_entry(self_ino);
+		if (!tx->mkreg(f_name, f_inode, time))
+			break;
+	}
+}
+
+void journal::rmreg(const uuid &self_ino, const std::string &f_name, std::shared_ptr<inode> f_inode, const struct timespec &time)
+{
+	while (true) {
+		auto tx = jtable.get_entry(self_ino);
+		if (!tx->rmreg(f_name, f_inode, time))
+			break;
+	}
+}
---
+++ client/journal/journal.hpp
@@ -0,0 +1,33 @@
+#ifndef _JOURNAL_HPP_
+#define _JOURNAL_HPP_
+
+#include <thread>
+
+#include "checkpoint.hpp"
+#include "commit.hpp"
+#include "journal_table.hpp"
+#include "mqueue.hpp"
+#include "../lease/lease_client.hpp"
+
+class journal {
+private:
+	rados_io *meta;
+	std::shared_ptr<lease_client> lc;
+
+	bool stopped;
+	journal_table jtable;
+	mqueue<std::shared_ptr<transaction>> q;
+	std::unique_ptr<std::thread> commit_thr, checkpoint_thr;
+
+public:
+	journal(rados_io *meta_pool, std::shared_ptr<lease_client> lease);
+	~journal(void);
+
+	void set_inode(const uuid &self_ino, std::shared_ptr<inode> i);
+	void mkdir(const uuid &self_ino, const std::string &d_name, const uuid &d_ino, const struct timespec &time);
+	void rmdir(const uuid &self_ino, const std::string &d_name, const uuid &d_ino, const struct timespec &time);
+	void mkreg(const uuid &self_ino, const std::string &f_name, std::shared_ptr<inode> f_inode, const struct timespec &time);
+	void rmreg(const uuid &self_ino, const std::string &f_name, std::shared_ptr<inode> f_inode, const struct timespec &time);
+};
+
+#endif /* _JOURNAL_HPP_ */
