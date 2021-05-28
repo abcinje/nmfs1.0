5ed0c73a877d92cc8503b8de31e9db96a834b03a
Injae Kang <abcinje@gmail.com>
2021-05-28 15:35:57 +0900

    Add checkpoint::checkpoint()

--- client/journal/checkpoint.cpp
+++ client/journal/checkpoint.cpp
@@ -1,5 +1,9 @@
 #include "checkpoint.hpp"
 
+checkpoint::checkpoint(rados_io *meta_pool, mqueue<std::shared_ptr<transaction>> *queue) : meta(meta_pool), q(queue)
+{
+}
+
 void checkpoint::operator()(void)
 {
 	while (true) {
