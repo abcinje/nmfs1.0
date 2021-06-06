26ea8d4b645882c757d1ac964148ade7c93eb484
CHOKYUJIN <bori19960@snu.ac.kr>
2021-06-06 16:14:18 +0900

    Fix uuid_controller logs

--- client/meta/uuid_controller.cpp
+++ client/meta/uuid_controller.cpp
@@ -6,7 +6,6 @@ uuid uuid_controller::alloc_new_uuid() {
 }
 
 uint64_t uuid_controller::get_prefix_from_uuid(const uuid& id) {
-	global_logger.log(inode_ops, "Called get_prefix_from_uuid(" + uuid_to_string(id) + ")");
 	uint64_t prefix;
 	const uint8_t* cursor = id.data;
 	memcpy(&prefix, cursor, sizeof(uint64_t));
@@ -15,7 +14,6 @@ uint64_t uuid_controller::get_prefix_from_uuid(const uuid& id) {
 }
 
 uint64_t uuid_controller::get_postfix_from_uuid(const uuid& id){
-	global_logger.log(inode_ops, "Called get_postfix_from_uuid(" + uuid_to_string(id) + ")");
 	uint64_t postfix;
 	const uint8_t* cursor = id.data + 8;
 	memcpy(&postfix, cursor, sizeof(uint64_t));
@@ -24,7 +22,6 @@ uint64_t uuid_controller::get_postfix_from_uuid(const uuid& id){
 }
 
 uuid uuid_controller::splice_prefix_and_postfix(const uint64_t prefix, const uint64_t postfix){
-	global_logger.log(inode_ops, "Called get_splice_from_uuid()");
 	uuid spliced_uuid{};
 	uint8_t* cursor = spliced_uuid.data;
 	memcpy(cursor, &prefix, sizeof(uint64_t));
@@ -36,6 +33,7 @@ uuid uuid_controller::splice_prefix_and_postfix(const uint64_t prefix, const uin
 }
 
 uuid get_root_ino(){
+	global_logger.log(inode_ops, "Called get_root_ino()");
 	/* root_ino = nil_uuid + 1 */
 	uuid root_ino = nil_uuid();
 	root_ino.data[15] = root_ino.data[15] + static_cast<uint8_t>(1);
