9c07a4e69b697da88ce786debd73d5eefcd5c7d1
Injae Kang <abcinje@gmail.com>
2021-07-16 23:50:27 +0900

    Use libconfig library

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ## Prerequisites
 Run the command to install required packages:
 ```
-sudo apt install meson cmake fuse3 libfuse3-dev libglib2.0-dev libboost-all-dev libradospp-dev
+sudo apt install meson cmake fuse3 libfuse3-dev libglib2.0-dev libboost-all-dev libradospp-dev libconfig++-dev
 ```
 And then, follow the [instruction](https://grpc.io/docs/languages/cpp/quickstart/) to install grpc.
 
--- manager/main.cpp
+++ manager/main.cpp
@@ -2,24 +2,54 @@
 #include <memory>
 #include <string>
 
+#include <libconfig.h++>
+
 #include "lib/rados_io/rados_io.hpp"
 
 #include "lease/lease_impl.hpp"
 #include "session/session_impl.hpp"
 
-#define META_POOL "nmfs.meta"
+using namespace libconfig;
+
+template <typename T>
+static T lookup_config(const Config &config, const char *field)
+{
+	try {
+		return config.lookup(field);
+	} catch (const SettingNotFoundException &e) {
+		std::cerr << "No \'" << field << "\' setting in configuration file." << std::endl;
+		exit(1);
+	}
+}
 
 int main(int argc, char *argv[])
 {
-	if (argc < 3) {
-		std::cerr << "Usage: " << argv[0] << " <manager_ip> <manager_port>" << std::endl;
+	if (argc < 2) {
+		std::cerr << "Usage: " << argv[0] << " <config_path>" << std::endl;
 		return 1;
 	}
 
-	rados_io::conn_info ci = {"client.admin", "ceph", 0};
-	auto meta_pool = std::make_shared<rados_io>(ci, META_POOL);
+	/* Get configuration */
 
-	std::string server_address(std::string(argv[1]) + ":" + std::string(argv[2]));
+	Config cfg;
+
+	try {
+		cfg.readFile(argv[1]);
+	} catch (const FileIOException &e) {
+		std::cerr << "I/O error while reading file." << std::endl;
+		exit(1);
+	}
+
+	std::string manager_ip = lookup_config<std::string>(cfg, "manager_ip");
+	std::string manager_port = lookup_config<std::string>(cfg, "manager_port");
+	std::string meta_pool_name = lookup_config<std::string>(cfg, "meta_pool_name");
+
+	/* Launch service */
+
+	rados_io::conn_info ci = {"client.admin", "ceph", 0};
+	auto meta_pool = std::make_shared<rados_io>(ci, meta_pool_name);
+
+	std::string server_address(manager_ip + ":" + manager_port);
 	lease_impl lease_service;
 	session_impl session_service(meta_pool);
 
